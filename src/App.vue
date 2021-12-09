4b49a840e8cc1ecb440feaedd7e69ad62807a53b
Jerome <jerome0caruso@gmail.com>
2021-12-09 09:17:00 -0600

    first push with start

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,47 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import axios from 'axios'
+const API_KEY = 'd5cdaac83979c51ad4c168671ad8f76b40200e50'
+
+// function sendData(crypto) {
+//   console.log(crypto)
+
+//     axios.get("http://localhost:5000")
+//       .then((response) => {
+//         console.log(response);
+//       })
+//       .catch((error) =>{
+//         alert(error);
+//       });
+
+// }
+function filterData(crypto) {
+  crypto.data.map(curr => {
+    console.log(curr.name)
+  })
+}
 export default {
+  
+  setup() {
+
+     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&ids=SOL,XTZ, ALGO&interval=1&per-page=100&page=1`)
+      .then((response) => {
+        filterData(response)
+        console.log(response);
+      })
+      .catch((error) =>{
+        alert(error);
+      });
+  }
+}
+/*export default {
   name: 'App',
   components: {
     HelloWorld
   }
-}
+}*/
 </script>
 
 <style>
