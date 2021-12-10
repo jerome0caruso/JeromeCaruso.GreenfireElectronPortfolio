52922533a36ba328cd76d494fa2ecd090d079bd4
Jerome <jerome0caruso@gmail.com>
2021-12-10 15:13:41 -0600

    urls now display with css

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld currencyList={currencyList} />
+  <div class="container">
+    <h1>Crypto Prices</h1>
+    <HelloWorld :currencyList="currencyList" />
+  </div>
 
 </template>
 
 <script>
-
 import axios from 'axios'
 import HelloWorld from './components/HelloWorld.vue'
 const API_KEY = 'd5cdaac83979c51ad4c168671ad8f76b40200e50'
-const currencyList = []
-function sendData(crypto) {
-  const jsonData = JSON.stringify(crypto)
-    axios.post("http://localhost:5000", jsonData, {
-      headers: {'Content-Type': 'application/json'}
-  })
+
+export default {
+  components: {
+    HelloWorld,
+ },
+ data() {
+   return {
+    currencyList: [],
+   }
+ },
+   async created() {
+    try {
+      const response = await axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&ids=SOL,XTZ, ALGO&interval=1&per-page=100&page=1`)
+      const filteredData = this.filterData(response);
+      this.sendData(filteredData)
+    } catch(error) {
+      console.log(error);
+    }
+  },
+
+methods: {
+  sendData(crypto) {
+    const jsonData = JSON.stringify(crypto)
+      axios.post("http://localhost:5000", jsonData, {
+        headers: {'Content-Type': 'application/json'}
+    })
     .then((response) => {
       console.log(response.data)
-      currencyList.push(response.data) 
+      this.currencyList = response.data
     })
     .catch((error) =>{
       alert(error, "error")
     })
+  },
 
+  filterData(crypto) {
+    const allData = []
+    const myName = {
+      name: "Jerome Caruso",
+      price: 0.00,
+      logo: '',
+      id: "JC"
+      }
+    crypto.data.map((curr, index) => {
+      const cryptoData = {}
+        cryptoData.name = curr.name
+        cryptoData.price = Number(curr.price).toFixed(2)
+        cryptoData.logo = curr.logo_url
+        cryptoData.id = curr.name + index
+      allData.push(cryptoData)
+    })
+    allData.push(myName)
+    return allData
+  },
 }
-function filterData(crypto) {
-  const allData = []
-  crypto.data.map(curr => {
-    const cryptoData = {}
-      cryptoData[curr.name] = curr.name
-      cryptoData.price = Number(curr.price).toFixed(2)
-      cryptoData.logo = curr.logo_url
-    allData.push(cryptoData)
-  })
-  sendData(allData)
-}
-
-export default {
-  components: {
-    HelloWorld
- },
-  setup() {
-     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&ids=SOL,XTZ, ALGO&interval=1&per-page=100&page=1`)
-      .then((response) => {
-        filterData(response)
-        console.log('Crypto response', response);
-      })
-      .catch((error) =>{
-        alert(error);
-      });
-  }
-}
-
-
-
+};
 
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -65,5 +78,48 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+  background: rgb(127, 193, 196);
+  height: 90vh;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,32 @@
 <template>
-  <div v-for="current of currencyList" :key="item.id">
+<div>
+  <div class="eachCry" v-for="currency of currencyList" :key="currency.id">
     <h1>{{currency.name}}</h1>
-    <img v-bind:src="logo_url"/>
+    <img :src="currency.qrCode"/>
+    <h2>{{'$' + currency.price}}</h2>
   </div>
+</div>
 </template>
 
 <script>
-console.log("hello componet")
 export default {
   name: 'HelloWorld',
   props: {
     currencyList: Array,
-  }
+  },
+   data() {
+    return {
+      name: "jerry",
+   }
+ },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h2 {
+  margin-top: 10px;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -30,5 +40,23 @@ li {
 }
 a {
   color: #42b983;
+}
+div {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  background-color: rgb(255, 181, 85);
+  margin: 0!important;
+  border: 3px solid white;
+}
+.eachCry {
+  flex-direction: column;
+  align-items: center;
+  max-width: 200px;
+  width: 200px;
+  border: 0;
+}
+.eachCry:last-child {
+  background-color: rgb(255, 145, 0);;
 }
 </style>
